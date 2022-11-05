350c8ae39b6d7126f16635a8ae33bf8fc3bb2b55
xe11us <vasilyev-av22@mail.ru>
2022-11-05 17:03:25 +0300

    Update HTMLUtils

--- java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/utils/HTMLUtils.java
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/utils/HTMLUtils.java
@@ -2,12 +2,15 @@ package ru.akirakozov.sd.refactoring.utils;
 
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Objects;
 
 public class HTMLUtils {
     public static void writeHTMLDocument(PrintWriter writer, List<String> headers, String text) {
         writer.println("<html><body>");
         headers.forEach(header -> writer.println("<h1>" + header + "</h1>"));
-        writer.println(text);
+        if (!Objects.isNull(text) && text.length() > 0) {
+            writer.println(text);
+        }
         writer.println("</body></html>");
     }
 }
